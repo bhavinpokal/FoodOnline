ca3cf2e187b60e21c8c0bc01dd7117bafc162f40
bhavinpokal <bhavinpokal@live.com>
2022-12-26 12:05:13 +0530

    User Registration and Homepage

--- FoodOnline_Main/settings.py
+++ FoodOnline_Main/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+from django.contrib.messages import constants as messages
 from pathlib import Path
 from decouple import config
 
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -86,6 +88,10 @@ DATABASES = {
 }
 
 
+# Registering custom user manager model
+AUTH_USER_MODEL = 'accounts.User'
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -126,7 +132,21 @@ STATICFILES_DIRS = [
     'FoodOnline_Main/static'
 ]
 
+
+# Media files
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
---
+++ accounts/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, UserProfile
+
+
+class CustomUserAdmin(UserAdmin):
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+    list_display = ('email', 'first_name', 'last_name', 'role', 'is_active')
+    ordering = ('-date_joined',)
+
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(UserProfile)
