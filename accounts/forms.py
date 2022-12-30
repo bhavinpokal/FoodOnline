dcdd1054922a3038b910e8678f5529a4ab740c95
bhavinpokal <bhavinpokal@live.com>
2022-12-30 11:36:46 +0530

    Vendor profile form and validations

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from accounts.models import User
+from accounts.models import User, UserProfile
+from accounts.validators import images_validator
 
 
 class UserForm(forms.ModelForm):
@@ -16,3 +17,28 @@ class UserForm(forms.ModelForm):
         confirm_password = cleaned_data.get('confirm_password')
         if password != confirm_password:
             raise forms.ValidationError('Password does not match.')
+
+
+class UserProfileForm(forms.ModelForm):
+    address = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Start typing...', 'required': 'required'}))
+    profile_picture = forms.FileField(
+        widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[images_validator])
+    cover_photo = forms.FileField(
+        widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[images_validator])
+    latitude = forms.CharField(
+        widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+    longitude = forms.CharField(
+        widget=forms.TextInput(attrs={'readonly': 'readonly'}))
+
+    class Meta:
+        model = UserProfile
+        fields = ['profile_picture', 'cover_photo', 'address', 'country',
+                  'state', 'city', 'pin_code', 'latitude', 'longitude']
+
+    # # Another way to make latitude and longitude readonly
+    # def __init__(self, *args, **kwargs):
+    #     super(UserProfileForm, self).__init__(*args, **kwargs)
+    #     for field in self.fields:
+    #         if field == 'latitude' or field == 'longitude':
+    #             self.fields[field].widget.attrs['readonly'] = 'readonly'
--- vendor/forms.py
+++ vendor/forms.py
@@ -2,9 +2,13 @@ from dataclasses import fields
 from pyexpat import model
 from django import forms
 from vendor.models import Vendor
+from accounts.validators import images_validator
 
 
 class VendorForm(forms.ModelForm):
+    vendor_license = forms.FileField(
+        widget=forms.FileInput(attrs={'class': 'btn btn-info'}), validators=[images_validator])
+
     class Meta:
         model = Vendor
         fields = ['vendor_name', 'vendor_license']
